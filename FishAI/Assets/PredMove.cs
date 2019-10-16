8f378802763716f17d7a0766fffd7ae034330df1
johnnymelin <johnny.melin95@gmail.com>
2019-10-16 16:17:58 +0200

    Added movement for predator

---
+++ FishAI/Assets/PredMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PredMove : MonoBehaviour
+{
+    public float predSpeed = 0.02f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.transform.position = this.transform.forward * predSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += this.transform.forward * predSpeed;
+        if (Vector3.Distance(this.transform.position, Vector3.zero) >= FlockManager.tankSize * 3.5)
+        {
+            this.transform.forward = -this.transform.forward;
+        }
+    }
+}
